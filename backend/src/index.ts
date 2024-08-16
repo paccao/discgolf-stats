1cc61bf795411f0162d00251ff797222cd260e57
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2024-08-16 22:48:57 +0200

    Added sample code to test prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,30 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const server = fastify()
+
+const prisma = new PrismaClient()
+
+async function main() {
+  try {
+    const query = await prisma.course.count()
+  console.log(query);
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+  })
+
+
 
 server.get('/ping', async (request, reply) => {
     return 'pong\n'
