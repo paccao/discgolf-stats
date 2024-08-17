73bffe1eb02b92fb37ca3b0b2e41ea5b6f2fd6d7
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2024-08-17 15:21:38 +0200

    Add zod type provider

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,19 @@
-import fastify from 'fastify'
+import Fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
-export const server = fastify({
-  logger: {
-    level: 'info',
-  },
-})
+import courseRoutes from './modules/course/routes'
+
+export const server = Fastify({
+  logger: { level: 'info' },
+}).withTypeProvider<ZodTypeProvider>()
 
 function buildServer() {
-  server.get('/healthcheck', async function () {
-    return { status: 'OK' }
-  })
+  server.setValidatorCompiler(validatorCompiler)
+  server.setSerializerCompiler(serializerCompiler)
 
   // register zod schemas
   //   for (const schema of [...]) {}
@@ -17,6 +21,12 @@ function buildServer() {
   // register openapi
 
   // register routes
+  server.register(courseRoutes, { prefix: 'api/courses' })
+
+  server.get('/healthcheck', async function () {
+    return { status: 'OK' }
+  })
+
   return server
 }
 
