9df7f68b729a00ef07752e333d091c527913c98e
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2024-08-20 23:43:06 +0200

    Cleanup seed script

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -71,11 +71,10 @@ async function main() {
 }
 
 main()
-  .then(() => {
-    prisma.$disconnect()
-  })
-  .catch(async (e) => {
+  .catch((e) => {
     console.error(e)
-    await prisma.$disconnect()
     process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
   })
