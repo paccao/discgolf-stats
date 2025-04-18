8f9f40a7b204c18f0860962c3bd83408faaf0efd
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-04-18 23:45:05 +0200

    Default to dark mode

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,10 +4,4 @@
   let count = $state(1)
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
-
 <Button onclick={() => count++}>Hello {count}</Button>
