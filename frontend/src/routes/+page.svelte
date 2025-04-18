1cecedfdeffe7701ac194b3a4115cc12762173d6
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-04-18 23:14:15 +0200

    Setup shadcn-svelte with Tailwind 4

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+  import Button from '$lib/components/ui/button/button.svelte'
+
+  let count = $state(1)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<Button onclick={() => count++}>Hello {count}</Button>
