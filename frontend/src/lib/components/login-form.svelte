6d6fd28548c3890ded3f38264713a87da7a6b44c
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-04-20 09:41:57 +0200

    Add login form

---
+++ frontend/src/lib/components/login-form.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button/index.js'
+  import * as Card from '$lib/components/ui/card/index.js'
+  import { Input } from '$lib/components/ui/input/index.js'
+  import { Label } from '$lib/components/ui/label/index.js'
+</script>
+
+<Card.Root class="mx-auto max-w-sm">
+  <Card.Header>
+    <Card.Title class="text-2xl">Login</Card.Title>
+  </Card.Header>
+  <Card.Content>
+    <div class="grid gap-4">
+      <div class="grid gap-2">
+        <Label for="username">Username</Label>
+        <Input id="username" type="text" required />
+      </div>
+      <div class="grid gap-2">
+        <div class="flex items-center">
+          <Label for="password">Password</Label>
+          <!-- TODO: implement password reset -->
+          <!-- <a href="##" class="ml-auto inline-block text-sm underline">
+            Forgot your password?
+          </a> -->
+        </div>
+        <Input id="password" type="password" required />
+      </div>
+      <Button type="submit" class="w-full">Login</Button>
+    </div>
+    <div class="mt-4 text-center text-sm">
+      <a href="/sign-up" class="px-2 underline">Sign up</a>
+    </div>
+  </Card.Content>
+</Card.Root>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import '../app.css'
 
-  export const ssr = false
-
   let { children } = $props()
 </script>
 
